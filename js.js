2aea3ca0896f5a5264bb2a30dd46392ac09fee8e
Roman Veselovsky <romanveselovsky@gmail.com>
2017-07-20 21:31:05 +0300

    HW10-JS-Inheritance: fixed

--- js.js
+++ js.js
@@ -1,64 +1,64 @@
-var Gladiator = function(name) {
+var Gladiator = function (name) {
     this.name = name;
     this.attack = '';
     this.hitpoints = '';
 };
 
-Gladiator.prototype.getAttack = function() {
+Gladiator.prototype.getAttack = function () {
     this.attack = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
 };
 
-Gladiator.prototype.setHitpoints = function() {
+Gladiator.prototype.setHitpoints = function () {
     this.hitpoints = Math.floor(Math.random() * (100 - 80 + 1)) + 80;
+
     console.log(this.name + ' hitpoints - ' + this.hitpoints);
 };
 
-Gladiator.prototype.updateSkills = function() {
+Gladiator.prototype.updateSkills = function () {
     this.getAttack();
     this.setHitpoints();
 };
 
-Gladiator.prototype.fight = function(contestant) {
+Gladiator.prototype.fight = function (contestant) {
     this.contestant = contestant;
 
-    if(contestant.hitpoints > 0 && gameOver === false && this.hitpoints > 0) {
+    if (contestant.hitpoints > 0 && gameOver === false && this.hitpoints >
+        0) {
         this.getAttack();
         contestant.hitpoints -= this.attack;
         this.fightLog();
-    } else this.winResult();
-};
-
-Gladiator.prototype.fightLog = function() {
-    if(this.contestant.hitpoints > 0) {
-        console.log(
-            this.name + ' attacked ' + this.contestant.name + ' for ' +
-            this.attack + ' damage. ' + this.contestant.name +
-            ' hitpoints are ' + this.contestant.hitpoints
-        );
     } else {
-        console.log(
-            this.name + ' attacked ' + this.contestant.name + ' for ' +
-            this.attack + ' damage. ' + this.contestant.name +
-            ' lose! '
-        );
+        this.winResult();
     }
 };
 
-Gladiator.prototype.winResult = function() {
-    if(this.contestant.hitpoints <= 0) {
+Gladiator.prototype.fightLog = function () {
+    if (this.contestant.hitpoints > 0) {
+        console.log(this.name + ' attacked ' + this.contestant.name +
+            ' for ' + this.attack + ' damage. ' + this.contestant.name +
+            ' hitpoints are ' + this.contestant.hitpoints);
+    } else {
+        console.log(this.name + ' attacked ' + this.contestant.name +
+            ' for ' + this.attack + ' damage. ' + this.contestant.name +
+            ' lose! ');
+    }
+};
+
+Gladiator.prototype.winResult = function () {
+    if (this.contestant.hitpoints <= 0) {
         gameOver = true;
         console.log(this.name + ' won!');
     }
 };
 
-var Champion = function(name) {
+var Champion = function (name) {
     Gladiator.call(this, name);
 };
 
 Champion.prototype = Object.create(Gladiator.prototype);
 Champion.prototype.constructor = Champion;
 
-var Monster = function(name) {
+var Monster = function (name) {
     Gladiator.call(this, name);
 };
 
@@ -74,7 +74,7 @@ function startNewGame() {
     Superman.updateSkills();
     Doomsday.updateSkills();
 
-    while(gameOver === false) {
+    while (gameOver === false) {
         Superman.fight(Doomsday);
         Doomsday.fight(Superman);
     }
